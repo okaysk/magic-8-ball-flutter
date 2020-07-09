c18d4ae687b17ae3481f236b5906f8832591687a
sk-weepet <67887045+sk-weepet@users.noreply.github.com>
2020-07-09 11:20:30 +0900

    Stateful + Stateless + Random

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text(
+          'Ask me Anything',
+          // textAlign: TextAlign.left,
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  void changeTheBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          changeTheBall();
+          print('$ballNumber');
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+      // child: Text('Hello'),
+    );
+  }
+}
